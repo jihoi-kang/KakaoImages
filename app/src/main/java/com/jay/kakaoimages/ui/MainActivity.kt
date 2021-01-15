7d03add71c3a64e5b3812f39af1f549a3a6187fb
Jihoi-Kang <jihoi.kang@gmail.com>
2021-01-15 15:28:28 +0900

    Change MainActivity directory

---
+++ app/src/main/java/com/jay/kakaoimages/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jay.kakaoimages.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jay.kakaoimages.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
